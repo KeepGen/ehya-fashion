99c93434be1dc004b90bdfcfd002f5e19b1fc339
KeepGen <keeper.gen@gmail.com>
2020-08-12 19:01:50 +0300

    create: added slider to testimonials section

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    autoplay: {
+      delay: 7000,
+    },
+    loop: true,
+    disableOnInteraction: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+
+
+  var mySwiper = document.querySelector('.swiper-container').swiper
+  $(".swiper-container").mouseenter(function () {
+    mySwiper.autoplay.stop();
+  });
+
+  $(".swiper-container").mouseleave(function () {
+    mySwiper.autoplay.start();
+  });
+});
